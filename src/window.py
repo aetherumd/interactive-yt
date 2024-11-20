a1f9da26af9f5878240765c2f80b2039e93b3bf1
Jonathan Loch <jjayaput@terpmail.umd.edu>
2024-11-20 18:27:23 -0500

    PlotMakerPanel now makes slice plots with directions x, y, z

--- src/window.py
+++ src/window.py
@@ -17,9 +17,11 @@ class YTWindow(QWidget):
             "sliceplot": True, # True -> slice plot, False -> projection plot 
             "direction": [1,0,0],
             "width": 100, 
-            "datasource": None,
+            "data_source": None,
             "ds_fields": [],
-            "imagepath": "tmp.png",
+            "ds_center": (0,0,0),
+            "field": [("ramses", "HeII")],
+            "image_path": "tmp.png",
         }
 
     def __init_layout__(self):
@@ -42,7 +44,12 @@ class YTWindow(QWidget):
         tab_menu.addTab("edit plot")
 
         tab_menu.tabBarClicked.connect(self.tab_switch)
+
         scroll_area = self.widgets["scroll_area"] = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+
+        scroll_area_widget = self.widgets["scroll_area_widget"] = PlotMakerPanel(self)
+        scroll_area.setWidget(scroll_area_widget)
 
         action_bar = self.widgets["action_bar"] = QWidget()
         action_bar_layout = QHBoxLayout(action_bar)
@@ -67,16 +74,22 @@ class YTWindow(QWidget):
         
         self.widgets["left"].setFixedWidth(self.width()//2-1)
         self.widgets["left"].setFixedHeight(self.width()//2-1)
+        
+        self.tab_switch(0)
 
     def resizeEvent(self, event):
         super().resizeEvent(event)
+        """
         self.widgets["left"].setFixedWidth(event.size().width()//2)
         self.widgets["left"].setFixedHeight(event.size().width()//2)
-        
-
+        """
+        self.widgets["right"].setMaximumWidth(event.size().width() - self.widgets["left"].width())
+        self.widgets["scroll_area"].setMaximumWidth(event.size().width() - self.widgets["left"].width())
+        #self.widgets["scroll_area_widget"].setMaximumWidth(event.size().width() - self.widgets["left"].width())
+        self.widgets["scroll_area_widget"].resizeEvent(event)
     def set_image(self, image_path=""):
         if image_path == "":
-            self.widgets["image"].setPixmap(QPixmap.fromImage(QImage(self.get_attribute("imagepath"))))
+            self.widgets["image"].setPixmap(QPixmap.fromImage(QImage(self.get_attribute("image_path"))))
         else:
             self.widgets["image"].setPixmap(QPixmap.fromImage(QImage(image_path)))
 
@@ -93,6 +106,7 @@ class YTWindow(QWidget):
         match i:
             case 0:
                 plot_maker = self.widgets["scroll_area_widget"] = PlotMakerPanel(self)
+                self.widgets["scroll_area"].setWidget(plot_maker)
                 pb = self.widgets["plot_button"]
                 try:
                     pb.clicked.disconnect()
@@ -103,11 +117,13 @@ class YTWindow(QWidget):
 
             case 1:
                 plot_editor = self.widgets["scroll_area_widget"] = PlotEditorPanel(self)
+                self.widgets["scroll_area"].setWidget(plot_editor)
                 pb = self.widgets["plot_button"]
                 try:
                     pb.clicked.disconnect()
                     pb.clicked.connect(plot_editor.update_fields)
                     pb.clicked.connect(self.plot)
+                    
                 except:
                     print("plot_button connect failed")
 
@@ -115,6 +131,8 @@ class YTWindow(QWidget):
     @QtCore.Slot()
     def plot(self):
         if self.get_attribute("data_source") != None:
+            plot_maker = self.widgets["scroll_area_widget"]
+            plot_maker.update_fields()
             temp = yt.SlicePlot(self.get_attribute("data_source"), self.get_attribute("direction"), self.get_attribute("field"))
             temp.save(self.get_attribute("image_path"))
             self.set_image(self.get_attribute("image_path"))
@@ -126,8 +144,9 @@ class YTWindow(QWidget):
         s = f.filesSelected()[0]
         if (s != None):
             ds = yt.load(s)
-            self.set_attribute("ds", ds)
-            self.set_attribute("fields", ds.field_list)
+            self.set_attribute("data_source", ds)
+            self.set_attribute("ds_fields", ds.field_list)
+            self.widgets["scroll_area_widget"].refresh()
 
 class FileDialog(QFileDialog):
     def __init__(self, *args):
@@ -161,11 +180,77 @@ class PlotMakerPanel(QWidget):
     
     def __init_layout__(self):
         self.widgets = {}
+        self.entries = []
+        
+        wgts = {
+            "Slice plot?": (IOOption.CHECKBOX, True, "sliceplot"),
+            "Direction": (IOOption.USER_ENTRY, "x", "direction"),
+            "Field": (IOOption.DROPDOWN, self.parent.get_attribute("ds_fields"), "field")
+        }
+
+        layout = QVBoxLayout(self)
+
+        for wgt_label in wgts.keys():
+            t, v, a = wgts[wgt_label]
+            entry = QWidget()
+            entry_layout = QHBoxLayout(entry)
+
+            w = QWidget()
+            match t:
+                case IOOption.DROPDOWN:
+                    w = QComboBox()
+                    w.addItems(v)
+                case IOOption.USER_ENTRY:
+                    w = QLineEdit()
+                    w.setText(v)
+                case IOOption.CHECKBOX:
+                    w = QCheckBox()
+                    w.setChecked(v)
+            
+            self.widgets[a] = (w, t, v)
+            entry_layout.addWidget(QLabel(wgt_label))
+            entry_layout.addWidget(w)
+
+            self.entries.append(entry)
+
+            layout.addWidget(entry)
+
+    def resizeEvent(self, event):
+        for entry in self.entries:
+            #entry.setFixedWidth(event.size().width()//2)
+            pass
+        
     
+    def refresh(self):
+        field_select, t, v = self.widgets["field"]
+        field_select.clear()
+        dsf = self.parent.get_attribute("ds_fields")
+        for f in dsf:
+            field_select.addItem(str(f))
+        self.widgets["field"] = (field_select, t, dsf)
+
     @QtCore.Slot()
     def update_fields(self):
-        pass
+        for wgt_label in self.widgets.keys():
+            w, t, v = self.widgets[wgt_label]
+            val = None
+            match t:
+                case IOOption.DROPDOWN:
+                    if len(v) > 0:
+                        val = v[w.currentIndex()]
+                    else:
+                        val = self.parent.get_attribute("field")
+                case IOOption.USER_ENTRY:
+                    val = w.text()
+                case IOOption.CHECKBOX:
+                    val = w.isChecked()
+            self.parent.set_attribute(wgt_label, val)
 
+class IOOption(Enum):
+    DROPDOWN = 0,
+    USER_ENTRY = 1,
+    CHECKBOX = 2
+    
 class PlotEditorPanel(QWidget):
     def __init__(self, parent):
         super().__init__(self)
@@ -178,8 +263,6 @@ class PlotEditorPanel(QWidget):
     @QtCore.Slot()
     def update_fields():
         pass
-
-
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
